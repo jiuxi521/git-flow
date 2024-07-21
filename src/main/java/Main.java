6223001b972902e5b12ed921dea49ade0ee329f5
mahao13 <1528128053@qq.com>
2024-07-21 09:32:18 +0800

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * 测试冲突
+ *
+ * @author mahao13
+ * @since 2024-07-21 09:29
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
